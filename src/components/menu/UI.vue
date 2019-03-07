96ce1b81a02fe2d3f811770c84fb418d95e7aa00
yonkov <dobrin.yonkov1@gmail.com>
2019-03-07 23:00:33 +0200

    feat: Add menu folder and component structure

---
+++ src/components/menu/UI.vue
@@ -0,0 +1,23 @@
+<template>
+    <component :is="component" v-bind="props"></component>
+</template>
+
+<script>
+  import {mapState} from 'vuex'
+
+  export default {
+    computed: {
+      ...mapState('layout', ['responsive']),
+      component() {
+        return this.responsive ? 'Mobile' : 'Desktop'
+      },
+      props() {
+        return {}
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
