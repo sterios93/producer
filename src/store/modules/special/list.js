12e97e1fc80400938a433c0746d8cb456812b0b2
sterios <sterios.taskudis@gmail.com>
2019-03-24 20:01:07 +0200

    fix: Add special offer card. Change flow.

--- src/store/modules/special/list.js
+++ src/store/modules/special/list.js
@@ -67,6 +67,204 @@ const state = () => ({
       schedule: null,
       startDate: '2019-09-10 12:00',
       endDate: '2019-10-10 12:00',
+    },
+    {
+      id: 0,
+      description: 'Only now in our Restaurant. Order one pizza and get the second one for free !',
+      discount: 10,
+      items: [
+        {
+          id: 11,
+          name: 'Lorem',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 0,
+            name: 'Cuban'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+        {
+          id: 0,
+          name: 'Lorem1',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 0,
+            name: 'Cuban'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+        {
+          id: 1,
+          name: 'Lorem2',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 0,
+            name: 'Cuban'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+        {
+          id: 2,
+          name: 'Lorem2',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 1,
+            name: 'Burger'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+      ],
+      name: 'Grande Pica Peperoni with Cheese',
+      picture: null,
+      image: 'https://api-content.prod.pizzahutaustralia.com.au//umbraco/api/Image/Get2?path=assets/products/menu/Veggie-Tandoori-LGE-Pizza-menu.jpg',
+      price: 160,
+      schedule: null,
+      startDate: '2019-09-10 12:00',
+      endDate: '2019-10-10 12:00',
+    },
+    {
+      id: 0,
+      description: 'Only now in our Restaurant. Order one pizza and get the second one for free !',
+      discount: 10,
+      items: [
+        {
+          id: 11,
+          name: 'Lorem',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 0,
+            name: 'Cuban'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+        {
+          id: 0,
+          name: 'Lorem1',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 0,
+            name: 'Cuban'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+        {
+          id: 1,
+          name: 'Lorem2',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 0,
+            name: 'Cuban'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+        {
+          id: 2,
+          name: 'Lorem2',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 1,
+            name: 'Burger'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+      ],
+      name: 'Grande Pica Peperoni with Cheese',
+      picture: null,
+      image: 'https://api-content.prod.pizzahutaustralia.com.au//umbraco/api/Image/Get2?path=assets/products/menu/Veggie-Tandoori-LGE-Pizza-menu.jpg',
+      price: 160,
+      schedule: null,
+      startDate: '2019-09-10 12:00',
+      endDate: '2019-10-10 12:00',
+    },
+    {
+      id: 0,
+      description: 'Only now in our Restaurant. Order one pizza and get the second one for free !',
+      discount: 10,
+      items: [
+        {
+          id: 11,
+          name: 'Lorem',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 0,
+            name: 'Cuban'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+        {
+          id: 0,
+          name: 'Lorem1',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 0,
+            name: 'Cuban'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+        {
+          id: 1,
+          name: 'Lorem2',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 0,
+            name: 'Cuban'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+        {
+          id: 2,
+          name: 'Lorem2',
+          image: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          price: 40,
+          isLunchOnly: false,
+          weight: 50,
+          category: {
+            id: 1,
+            name: 'Burger'
+          },
+          description: 'Roast chicken, baby carrots, spring peas topped with grandma’s flakey pie crust.',
+        },
+      ],
+      name: 'Grande Pica Peperoni with Cheese',
+      picture: null,
+      image: 'https://api-content.prod.pizzahutaustralia.com.au//umbraco/api/Image/Get2?path=assets/products/menu/Veggie-Tandoori-LGE-Pizza-menu.jpg',
+      price: 160,
+      schedule: null,
+      startDate: '2019-09-10 12:00',
+      endDate: '2019-10-10 12:00',
     }
   ],
 })
